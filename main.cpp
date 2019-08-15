8d127d9bc0eee017029cf250e961a7d810cd69d4
Kanthan Paheerathan <kanthan.paheerathan@arm.com>
2019-08-15 15:15:42 +0100

    Modified LED variable main.cpp file

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 #include "mbed.h"
 #include "stats_report.h"
 
-DigitalOut led1(LED1);
+DigitalOut led1(LED2);
 
 #define SLEEP_TIME                  500 // (msec)
 #define PRINT_AFTER_N_LOOPS         20
