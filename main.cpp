ab2d3916d404454eb3e07dda45490735f90fa93f
Kanthan Paheerathan <kanthan.paheerathan@arm.com>
2019-09-06 17:30:27 +0100

    Updated main.cpp: change LED colour & added printf
    Change LED colour to green by default and added printf state about LED colour to main() function

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 #include "mbed.h"
 #include "stats_report.h"
 
-DigitalOut led1(LED1);
+DigitalOut led1(LED2);
 
 #define SLEEP_TIME                  500 // (msec)
 #define PRINT_AFTER_N_LOOPS         20
@@ -21,6 +21,7 @@ int main()
         // Blink LED and wait 0.5 seconds
         led1 = !led1;
         wait_ms(SLEEP_TIME);
+        printf("Green LED blinking!!");
 
         if ((0 == count) || (PRINT_AFTER_N_LOOPS == count)) {
             // Following the main thread wait, report on the current system status
