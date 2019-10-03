91e6a1a0366f72c253b1e92cc4257fb49d71954b
KP <kanthan.paheerathan@arm.com>
2019-10-03 16:18:59 +0100

    Changed to Blue LED and updated to mbed-os.lib file to 5.13.4

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 #include "mbed.h"
 #include "stats_report.h"
 
-DigitalOut led1(LED2);
+DigitalOut led1(LED3);
 
 #define SLEEP_TIME                  3500 // (msec)
 #define PRINT_AFTER_N_LOOPS         20
@@ -21,7 +21,7 @@ int main()
         // Blink LED and wait 3.5 seconds
         led1 = !led1;
         wait_ms(SLEEP_TIME);
-        printf("Green LED blinking every 3500ms!!");
+        printf("Blue LED blinking every 3500ms!!");
 
         if ((0 == count) || (PRINT_AFTER_N_LOOPS == count)) {
             // Following the main thread wait, report on the current system status
