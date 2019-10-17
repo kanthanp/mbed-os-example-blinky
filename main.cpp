cd527eace4d9cf59717eec2671405a39e25f0db2
Kanthan Paheerathan <kanthan.paheerathan@arm.com>
2019-10-17 15:34:58 +0100

    Update main.cpp - Green LED
    changed to Green LED and updated printf statement

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 #include "mbed.h"
 #include "stats_report.h"
 
-DigitalOut led1(LED3);
+DigitalOut led1(LED_GREEN);
 
 #define SLEEP_TIME                  1000 // (msec)
 #define PRINT_AFTER_N_LOOPS         20
@@ -21,7 +21,7 @@ int main()
         // Blink LED and wait 1 second
         led1 = !led1;
         wait_ms(SLEEP_TIME);
-        printf("Blue LED blinking every 1000ms!!");
+        printf("Green LED blinking every 1000ms!!");
 
         if ((0 == count) || (PRINT_AFTER_N_LOOPS == count)) {
             // Following the main thread wait, report on the current system status
