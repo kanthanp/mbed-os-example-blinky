efe9f09a8162e0e03c81a090732462ab9ba82ef8
Kanthan Paheerathan <kanthan.paheerathan@arm.com>
2020-02-10 15:27:17 +0000

    LED to Red

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 #include "mbed.h"
 #include "stats_report.h"
 
-DigitalOut led1(LED_BLUE);
+DigitalOut led1(LED_RED);
 
 #define SLEEP_TIME                 2500 // (msec)
 #define PRINT_AFTER_N_LOOPS         20
@@ -23,7 +23,7 @@ int main()
         led1 = !led1;
         wait_ms(SLEEP_TIME);
         // KP: print blinky message in Serial Monitor
-        printf("Blue LED is blinking on device every 2.5 seconds!!! \n");
+        printf("Red LED is blinking on device every 2.5 seconds!!! \n");
 
         if ((0 == count) || (PRINT_AFTER_N_LOOPS == count)) {
             // Following the main thread wait, report on the current system status
