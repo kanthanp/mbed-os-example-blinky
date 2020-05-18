6a144f24f0aac1c6fb579509b758c281fa99479e
Kanthan Paheerathan <kanthan.paheerathan@arm.com>
2020-05-18 14:04:27 +0100

    Changed LED to GREEN in main.cpp file
    Also updated printf statement

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 #include "mbed.h"
 #include "stats_report.h"
 
-DigitalOut led1(LED1);
+DigitalOut led1(LED_GREEN);
 
 #define SLEEP_TIME                 2000 // (msec)
 #define PRINT_AFTER_N_LOOPS         20
@@ -22,7 +22,7 @@ int main()
         // Blink LED and wait 2 seconds
         led1 = !led1;
         wait_ms(SLEEP_TIME);
-        printf("LED1 is blinking on device every 2 seconds!!! \n");
+        printf("GREEN LED is blinking on device every 2 seconds!!! \n");
 
         if ((0 == count) || (PRINT_AFTER_N_LOOPS == count)) {
             // Following the main thread wait, report on the current system status
