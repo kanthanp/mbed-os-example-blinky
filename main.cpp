76217ad07faf196511b0672d443ea7954771e3b1
Kanthan Paheerathan <kanthan.paheerathan@arm.com>
2020-09-21 12:32:29 +0100

    Reduced SLEEP TIME to 1sec in main.cpp file

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@
 
 DigitalOut led1(LED_GREEN);
 
-#define SLEEP_TIME                 3000 // (msec)
+#define SLEEP_TIME                 1000 // (msec)
 #define PRINT_AFTER_N_LOOPS         20
 
 // main() runs in its own thread in the OS
@@ -19,10 +19,10 @@ int main()
     int count = 0;
     while (true) {
 
-        // Blink LED and wait 3 seconds
+        // Blink LED and wait 1 second
         led1 = !led1;
         wait_ms(SLEEP_TIME);
-        printf("Green is blinking on device every 3 seconds!!! \n");
+        printf("Green is blinking on device every 1 second!! \n");
 
         if ((0 == count) || (PRINT_AFTER_N_LOOPS == count)) {
             // Following the main thread wait, report on the current system status
