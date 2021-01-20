41342607a293cd6c6da205cb1cbb4673c1bd6cd8
KP <kanthan.paheerathan@arm.com>
2021-01-20 17:33:53 +0000

    Set LED to RED in main.cpp

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 #include "mbed.h"
 #include "stats_report.h"
 
-DigitalOut led1(LED3);
+DigitalOut led1(LED_RED);
 
 #define SLEEP_TIME                 1500 // (msec)
 #define PRINT_AFTER_N_LOOPS         20
@@ -22,7 +22,7 @@ int main()
         // Blink LED and wait 1.5 seconds
         led1 = !led1;
         wait_ms(SLEEP_TIME);
-        printf("Blue LED is blinking on device every 1.5 seconds!! \n");
+        printf("Red LED is blinking on device every 1.5 seconds!! \n");
 
         if ((0 == count) || (PRINT_AFTER_N_LOOPS == count)) {
             // Following the main thread wait, report on the current system status
