2641298dc25972c60d41c67b5f9ea1c1d79912a3
KP <kanthan.paheerathan@arm.com>
2021-01-20 17:40:08 +0000

    SLEEP_TIME set to 2.5 sec in main.cpp

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@
 
 DigitalOut led1(LED_RED);
 
-#define SLEEP_TIME                 1500 // (msec)
+#define SLEEP_TIME                 2500 // (msec)
 #define PRINT_AFTER_N_LOOPS         20
 
 // main() runs in its own thread in the OS
@@ -19,10 +19,10 @@ int main()
     int count = 0;
     while (true) {
 
-        // Blink LED and wait 1.5 seconds
+        // Blink LED and wait 2.5 seconds
         led1 = !led1;
         wait_ms(SLEEP_TIME);
-        printf("Red LED is blinking on device every 1.5 seconds!! \n");
+        printf("Red LED is blinking on device every 2.5 seconds!! \n");
 
         if ((0 == count) || (PRINT_AFTER_N_LOOPS == count)) {
             // Following the main thread wait, report on the current system status
